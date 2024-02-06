ba76e374c8bdf75211c9f1560929dca500ad7b9b
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2024-02-06 23:19:18 +0100

    Make KGC functions vectorized

---
+++ docs/kgcPy2.py
@@ -0,0 +1,20 @@
+'''Vectorized version of functions in the kgcPy package'''
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = None
+
+import pandas as pd
+from importlib import resources
+
+with resources.open_binary('kgcPy', 'kmz_int_reshape.png') as fp:
+    kg_img = Image.open(fp)
+    kg_img.load()
+
+with resources.open_binary('kgcPy', 'kg_zoneNum.csv') as fp:
+    kg_df = pd.read_csv(fp)
+
+def lookupCZ(lat, lon):
+    x = round((lon+180)*(kg_img.size[0])/360 - 0.5)
+    y = round(-(lat-90)*(kg_img.size[1])/180 - 0.5)
+    num = kg_img.getpixel((x, y))
+    res = kg_df['kg_zone'].loc[kg_df['zoneNum'] == num]
+    return res.values[0]
