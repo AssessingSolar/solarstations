cc89fff7ecd69048e9c36473e9255d1a5981c460
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2024-10-28 23:49:22 +0100

    Add global annual dataset

--- scripts/nasa_power.py
+++ scripts/nasa_power.py
@@ -1,63 +1,75 @@
 """Functions to retrieve data from NASA POWER."""
 
 import pandas as pd
-import numpy as np
 import requests
-
+import numpy as np
 
 DEFAULT_PARAMETERS = [
     'ALLSKY_SFC_SW_DNI', 'ALLSKY_SFC_SW_DIFF', 'ALLSKY_SFC_SW_DWN']
 
-url = 'https://power.larc.nasa.gov/api/temporal/climatology/point'
+URL = 'https://power.larc.nasa.gov/api/temporal/climatology/regional?'
 
 
-def get_nasa_power_annual(latitude, longitude, community='RE',
-                          parameters=DEFAULT_PARAMETERS):
-    """
-    Retrieve data from NASA POWER.
+def get_nasa_power(latitude, longitude, start='1990-01-01', end='2009-01-01',
+                   community='RE', parameters=DEFAULT_PARAMETERS, url=URL):
 
-    Parameters
-    ----------
-    latitude : float
-        Latitude, north is positive. [degrees]
-    longitude : float
-        Longitude, east is positive. [degrees]
-    community : str, optional
-        Community type. The default is 'RE'.
-    parameters : TYPE, optional
-        List of weather parameters. The default is :const:`DEFAULT_PARAMETERS`.
+    latitude_min, latitude_max = latitude  # bottom, top
+    longitude_min, longitude_max = longitude  # left, right
 
-    Returns
-    -------
-    data : pd.DataFrame
-        Weather data
-    meta : dict
-        Dictionary with metadata.
-    """
     params = {
-        'latitude': latitude,
-        'longitude': longitude,
+        'start': pd.Timestamp(start).year,
+        'end': pd.Timestamp(end).year,
+        'latitude-min': latitude_min,
+        'latitude-max': latitude_max,
+        'longitude-min': longitude_min,
+        'longitude-max': longitude_max,
         'community': community,
         'parameters': ','.join(parameters),
-        'format': 'JSON',
+        'format': 'json',
         'user': 'DAVE',
     }
 
     res = requests.get(url, params=params)
+    res.raise_for_status()
 
-    data = pd.DataFrame(data=res.json()['properties']['parameter'])
+    meta = res.json()['header']
+    meta['parameters'] = res.json()['parameters']
 
-    days = np.array(
-        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 365])
-    # convert from kWh/m^2/day to kWh/m^2/yr
-    data = data.multiply(days, axis='rows')
+    data = pd.json_normalize(data=res.json()['features'])
+
+    data = data.replace(-999, np.nan)
 
     data = data.rename(columns={
-        'ALLSKY_SFC_SW_DWN': 'GHI_annual_kWh_m2',
-        'ALLSKY_SFC_SW_DNI': 'DNI_annual_kWh_m2',
-        'ALLSKY_SFC_SW_DIFF': 'DHI_annual_kWh_m2',
+        'properties.parameter.ALLSKY_SFC_SW_DWN.ANN': 'GHI',
+        'properties.parameter.ALLSKY_SFC_SW_DIFF.ANN': 'DHI',
+        'properties.parameter.ALLSKY_SFC_SW_DNI.ANN': 'DNI',
     })
-    data = data.rename(index={'ANN': 'Annual'})
-    data = data.loc['Annual', :]
-    meta = {}
+
+    data[['longitude', 'latitude']] = \
+        pd.DataFrame(data['geometry.coordinates'].to_list()).iloc[:, :-1]
+
+    data = data.set_index(['latitude', 'longitude'])
+
+    data = data[['GHI', 'DHI', 'DNI']]
+
+    data = data.multiply(365)  # convert kWh/m2/day to kWh/m2/yr
+
     return data, meta
+
+
+# %%
+step_size = 10
+latitudes = np.arange(-90, 90, step_size)
+longitudes = np.arange(-180, 180, step_size)
+
+dfs = []
+for lat in latitudes:
+    latitude = [lat, lat + step_size]
+    for lon in longitudes:
+        longitude = [lon, min(lon + step_size, 179)]
+        data, _ = get_nasa_power(latitude, longitude)
+        dfs.append(data)
+
+df = pd.concat(dfs, axis='rows')
+
+df.to_csv('data/nasa_power_annual_irradiance_global.csv')
