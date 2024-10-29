71162b1c4bb677d1c3bb088a7e4e700044549b6b
Adam R. Jensen <39184289+AdamRJensen@users.noreply.github.com>
2024-10-29 16:08:22 +0100

    Add background color to logo

--- scripts/logo.py
+++ scripts/logo.py
@@ -60,7 +60,7 @@ fig, ax = plt.subplots(figsize=(4, 4), subplot_kw={'projection': crs})
 
 # Add land borders, coastline, and gridlines to main map
 ax.add_feature(cartopy.feature.LAND, facecolor='lightgrey', alpha=0.7, zorder=0)
-ax.coastlines(edgecolor='black', lw=0.7, alpha=0.7, zorder=3)
+ax.coastlines(color='black', lw=0.7, alpha=1, zorder=3)
 ax.gridlines(draw_labels=False, dms=True, x_inline=False, y_inline=False, alpha=0.7, lw=0.1, zorder=-1)
 
 # Add points of solar stations
@@ -69,13 +69,13 @@ ax.gridlines(draw_labels=False, dms=True, x_inline=False, y_inline=False, alpha=
 stations.plot.scatter(
     ax=ax, x='Longitude', y='Latitude',
     c='Color', alpha=0.95, s=12, edgecolor='k', lw=0.2,
-    transform=ccrs.PlateCarree())
+    transform=ccrs.PlateCarree(), zorder=4)
 
 # Create the figure
 # ax.legend(handles=legend_elements, loc='lower left',
 #           frameon=False, bbox_to_anchor=[0, 0.2])
 
-ax.patch.set_facecolor('#fbf8f1')
+ax.patch.set_facecolor('lightyellow')#'#fbf8f1')
 
 fig.savefig('solarstations_map.png', dpi=900, bbox_inches='tight', facecolor="none")
 plt.show()
